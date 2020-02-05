b85bd3d65b32c45a3e8591b8c07c1cda8ab27c6f
madhu <madhusudhan@zibtek.in>
2020-02-06 02:02:16 +0530

    update script

--- precommit.sh
+++ precommit.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-npm test
+npm test > test.txt
 
-echo "hello"
+hello="$(grep  ^Statements[[:space:]]*:[[:space:]]*[0-9]* test.txt)"
 
+grep ^Statements[[:space:]]*:[[:space:]]*[0-9]*% test.txt > test2.txt
+
+ sed -i 's/Statements[[:space:]]*:[[:space:]]*//gi' test2.txt 
+ 
+ sed -i 's/%[[:space:]]*([[:space:]]*[0-9]*\/[0-9]*[[:space:]])//gi' test2.txt
+ 
+output="$(cat test2.txt)"
+
+
+if [ $output -gt 1000 ]
+then
 exit 0
+else
+exit 1
+fi
