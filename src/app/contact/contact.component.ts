e2189a9a732cfa1e85498468462c8d7347996a8f
madhu <madhusudhan@zibtek.in>
2020-02-17 12:25:30 +0530

    added linter rules

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,47 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-
   text = 'contact page1';
+
   contactForm: FormGroup;
+
   contact = {
-      name: '',
-      email: '',
-      text: ''
-      
+    name: '',
+    email: '',
+    text: '',
   };
+
   submitted = false;
 
   constructor() {
-    console.log("test");
-      this.createForm();
-      console.log("test");
+    this.createForm();
   }
 
   createForm(): void {
-
-    console.log("test");
-      this.contactForm = new FormGroup({
-          'name': new FormControl(this.contact.name, [
-              Validators.required,
-              Validators.minLength(4)
-          ]),
-          'email': new FormControl(this.contact.email, [
-              Validators.required,
-              Validators.email
-          ]),
-          'text': new FormControl(this.contact.text, Validators.required)
-      });
+    this.contactForm = new FormGroup({
+      name: new FormControl(this.contact.name, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      email: new FormControl(this.contact.email, [
+        Validators.required,
+        Validators.email,
+      ]),
+      text: new FormControl(this.contact.text, Validators.required),
+    });
   }
 
   onSubmit(): void {
-    console.log("test");
-      this.submitted = true;
+    this.submitted = true;
   }
 }
